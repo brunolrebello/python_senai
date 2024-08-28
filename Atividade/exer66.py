f21f710eedd6247168456f145c5a5505d1e0bbfa
Flávio <flaviomarques2012@gmail.com>
2024-08-28 17:55:50 -0300

    revisao

---
+++ Atividade/exer66.py
@@ -0,0 +1,10 @@
+#  Escreva um algoritmo que forneça um menu para o usuário: 1-Cadastrar nome, 2- atualizar o nome, 3, excluir, 
+# 4-listar todos os cadastrados, ao final da operação deve dar uma mensagem indicando o resultado da operação e perguntando se deseja realizar
+# outra operação, o seu aplicativo apenas deve encerrar quando a opção não for inserida.
+
+nome = str(input("""Digite 
+1-Cadastrar nome, 
+2- atualizar o nome, 
+3 excluir,
+4-listar todos os cadastrados
+"""))
